d0711ca31a0fce68f145a44afcbdd9b193ad299d
Pushkar988 <puskarkmr@gmail.com>
2018-01-07 20:54:51 +0530

    Add files via upload

--- srcmain/java/compushkar/controllers/practice/UserController.java
+++ srcmain/java/compushkar/controllers/practice/UserController.java
@@ -1,1 +1,31 @@
+package com.pushkar.controllers.practice;
 
+import com.pushkar.models.practice.User;
+import com.pushkar.services.practice.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    public UserService userService;
+
+    @RequestMapping(value = "/registration",method = RequestMethod.POST)
+    public @ResponseBody String registration(@RequestBody User user){
+        int i=userService.resgistration(user);
+        if(i>0){
+            return "successfully registered";
+        }else{
+            return "not registered";
+        }
+
+    }
+
+    @RequestMapping(value = "/users",method = RequestMethod.GET)
+    public List<User> getUsers(){
+
+        return userService.getUsers();
+    }
+}
